51551d1ea55be710bca1970455ea00b3ab1311a6
John Dyer <lbrapid@gmail.com>
2012-10-02 13:32:04 -0400

    Fix zoom button being replaced by thumbnail

--- app/assets/javascripts/store/zoom.js
+++ app/assets/javascripts/store/zoom.js
@@ -1,4 +1,6 @@
-var add_image_handlers = function() {
+var add_image_handlers, show_variant_images, update_variant_price;
+
+add_image_handlers = function() {
   // Remove duplicate image from gallery
   $("#main-image a.large-image").click(function(event) {
     $('#main-image a.click-to-zoom').attr('rel', '');
@@ -9,50 +11,52 @@ var add_image_handlers = function() {
     $('#main-image a.large-image').attr('rel', '');
   });
 
-  $("#main-image").data('selectedThumb', $('#main-image img').first().attr('src'));
-  $('ul.thumbnails li').eq(0).addClass('selected');
-
-  $('ul.thumbnails a').on('click', function(event) {
+  ($('#main-image')).data('selectedThumb', ($('#main-image img')).attr('src'));
+  ($('ul.thumbnails li')).eq(0).addClass('selected');
+  ($('ul.thumbnails a')).on('click', function(event) {
     // Append old image to gallery and remove new main-image from gallery to
     // prevent image duplication
     $('#gallery').append('<a class="fancybox" rel="gallery"><img src="' + $(".zoom-image").attr("href") + '" /></a>');
     $('#gallery a[href="' + $(this).data("original") + '"]').remove();
 
-    $("#main-image").data('selectedThumb', $(event.currentTarget).attr('href'));
-    $("#main-image").data('selectedThumbId', $(event.currentTarget).parent().attr('id'));
-    $('a.zoom-image').attr('href', $(this).data('original'));
-    $(this).mouseout(function() {
-      $('ul.thumbnails li').removeClass('selected');
-      $(event.currentTarget).parent('li').addClass('selected');
+    ($('#main-image')).data('selectedThumb', ($(event.currentTarget)).attr('href'));
+    ($('#main-image')).data('selectedThumbId', ($(event.currentTarget)).parent().attr('id'));
+    ($(this)).mouseout(function() {
+      ($('ul.thumbnails li')).removeClass('selected');
+      return ($(event.currentTarget)).parent('li').addClass('selected');
     });
     return false;
   });
+
   $('ul.thumbnails li').on('mouseenter', function(event) {
-    $('#main-image img').first().attr('src', $(event.currentTarget).find('a').attr('href'));
+    $('.click-to-zoom').attr('src', '/assets/zoom.gif');
+    // $('#main-image img').first().attr('src', $(event.currentTarget).find('a').attr('href'));
   });
   $('ul.thumbnails li').on('mouseleave', function(event) {
-    $('#main-image img').first().attr('src', $("#main-image").data('selectedThumb'));
+    $('.click-to-zoom').attr('src', '/assets/zoom.gif');
+    // $('#main-image img').first().attr('src', $("#main-image").data('selectedThumb'));
   });
 };
 
-var show_variant_images = function(variant_id) {
-  $('li.vtmb').hide();
-  $('li.vtmb-' + variant_id).show();
-  var currentThumb = $('#' + $("#main-image").data('selectedThumbId'));
-  // if currently selected thumb does not belong to current variant, nor to common images,
-  // hide it and select the first available thumb instead.
-  if(!currentThumb.hasClass('vtmb-' + variant_id) && !currentThumb.hasClass('tmb-all')) {
-    var thumb = $($('ul.thumbnails li:visible').eq(0));
-    var newImg = thumb.find('a').attr('href');
-    $('ul.thumbnails li').removeClass('selected');
+show_variant_images = function(variant_id) {
+  var currentThumb, newImg, thumb;
+  ($('li.vtmb')).hide();
+  ($('li.vtmb-' + variant_id)).show();
+  currentThumb = $('#' + ($('#main-image')).data('selectedThumbId'));
+  if (!currentThumb.hasClass('vtmb-' + variant_id) && !currentThumb.hasClass('tmb-all')) {
+    thumb = $(($('ul.thumbnails li:visible')).eq(0));
+    newImg = thumb.find('a').attr('href');
+    ($('ul.thumbnails li')).removeClass('selected');
     thumb.addClass('selected');
-    $('#main-image img').first().attr('src', newImg);
-    $("#main-image").data('selectedThumb', newImg);
-    $("#main-image").data('selectedThumbId', thumb.attr('id'));
+    ($('#main-image img')).attr('src', newImg);
+    ($('#main-image')).data('selectedThumb', newImg);
+    return ($('#main-image')).data('selectedThumbId', thumb.attr('id'));
   }
-}
+};
 
 $(document).ready(function() {
+    add_image_handlers();
+    $('.click-to-zoom').attr('src', '/assets/zoom.gif');
     $(".fancybox").fancybox({
       'beforeClose': function() {
         $('#main-image a.click-to-zoom').attr('rel', 'gallery');
