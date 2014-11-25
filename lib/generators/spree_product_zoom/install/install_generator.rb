0cf5930b8f1065b0b878deddb72b1685bab4b22d
Thomas K. Schmidt <ts@kruby.dk>
2014-11-25 19:20:57 +0100

    Update install_generator.rb

--- lib/generators/spree_product_zoom/install/install_generator.rb
+++ lib/generators/spree_product_zoom/install/install_generator.rb
@@ -3,11 +3,11 @@ module SpreeProductZoom
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/product_zoom\n"
+        append_file 'assets/javascripts/spree/frontend/all.js', "//= require spree/product_zoom\n"
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/product_zoom\n", :before => /\*\//, :verbose => true
+        inject_into_file 'assets/stylesheets/spree/frontend/all.css', " *= require spree/product_zoom\n", :before => /\*\//, :verbose => true
       end
     end
   end
